b20cfe7c3bcd4758b0af0eb274f29c05afdff8a2
Liudmyla Kirichenko <liudmyla.deleychuk@gmail.com>
2026-04-10 13:47:06 +0300

    creating pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,40 @@
+
+import styles from "./Pagination.module.css";
+
+const Pagination = ({
+  totalPages, 
+  handleNextPage, 
+  handlePreviousPage, 
+  handlePageClick, 
+  currentPage
+}) => {
+  return (
+  <div className={styles.pagination}>
+    <button onClick={handlePreviousPage}
+     className={styles.btn}
+     disabled={currentPage <= 1}
+     >{'<'}
+     </button>
+    <div className={styles.list}>
+      {[...Array(totalPages)].map((_, index) => (
+            <button onClick={() => {handlePageClick(index + 1)}} 
+            className={styles.pageNumber} 
+            key={index}
+            disabled={index + 1 === currentPage}
+            >
+              {index + 1}
+              </button>
+      ))}
+    </div>
+    <button 
+    onClick={handleNextPage} 
+    className={styles.btn}
+    disabled={currentPage >= totalPages}
+    >
+      {'>'}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
